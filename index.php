a04547ae266b0f0dc2e33d1d87eee4131f8fcdbb
Anniessa Antar <anniessa.antar@gmail.com>
2023-05-15 17:08:33 +0100

    testing with table data

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/*
+
+*	File:	practice-index.php	
+*	By:		Anniessa Antar
+*	Date:	2023-05-15	
+
+=====================================
+*/
+
+echo "This is the first index page";
+echo "<br />";
+echo "<br />";
+echo "by: Anniessa Antar";
+
+//      3 x 3 table with data
+echo "<table>";
+	echo "<tr>";
+		echo "<td>ID</td>";
+		echo "<td>Company</td>";
+		echo "<td>Postcode</td>";
+	echo "</tr>";
+	echo "<tr>";
+		echo "<td>201</td>";
+		echo "<td>TMIT</td>";
+		echo "<td>55407</td>";
+	echo "</tr>";
+	echo "<tr>";
+		echo "<td>334</td>";
+		echo "<td>Hello!</td>";
+		echo "<td>55414</td>";
+	echo "</tr>";
+echo "</table>";
+
+?>
